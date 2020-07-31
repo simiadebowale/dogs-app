7e3ee9e31ab5c3013af66c2e32fc0c4d8b5c1edb
Simi Adebowale <adebowalesimileoluwa@gmail.com>
2020-07-31 19:51:09 -0400

    Updates

---
+++ precache-manifest.e4ce43d553024e5d5b58ddd78663c246.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb543545f3dc3eb0c4ece78302d1492",
+    "url": "/dog-app/index.html"
+  },
+  {
+    "revision": "5d89aa312dbc94277c3a",
+    "url": "/dog-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2ebc6860bc844d4b2123",
+    "url": "/dog-app/static/css/main.fbe40a2d.chunk.css"
+  },
+  {
+    "revision": "5d89aa312dbc94277c3a",
+    "url": "/dog-app/static/js/2.6cf3f6cd.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/dog-app/static/js/2.6cf3f6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebc6860bc844d4b2123",
+    "url": "/dog-app/static/js/main.c7ec6efa.chunk.js"
+  },
+  {
+    "revision": "cbf6ae1a5e15235d6422",
+    "url": "/dog-app/static/js/runtime-main.eece9791.js"
+  },
+  {
+    "revision": "4eb85e80248f3371f721a9687e16d075",
+    "url": "/dog-app/static/media/hazel.4eb85e80.jpg"
+  },
+  {
+    "revision": "2f47cb659ae081c9e07bc25c264f0aed",
+    "url": "/dog-app/static/media/tubby.2f47cb65.jpg"
+  },
+  {
+    "revision": "5c1e3b17efc4e2197352567ea0e262c1",
+    "url": "/dog-app/static/media/whiskey.5c1e3b17.jpg"
+  }
+]);
